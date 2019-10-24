026bad029b831049753144fff1ea39e45c1a7d5b
Gabriel Silva Simões <simoes.sgabriel@gmail.com>
2019-10-24 19:47:03 -0400

    Translate a few codes and comments

--- code/cycle_cancel.cpp
+++ code/cycle_cancel.cpp
@@ -2,13 +2,13 @@
 //typedef int num; // type of cost
 //const int N = ; const int M =  * 2; const val eps = 0;
 struct cycle_cancel {
-	int hd[N], seen[N], qu[N], lv[N], ei[N], to[M], nx[M], ct[N], pai[N]; val fl[M], cp[M], flow; num cs[M], d[N], tot; int en = 2, n; int tempo = 0;
+	int hd[N], seen[N], qu[N], lv[N], ei[N], to[M], nx[M], ct[N], pai[N]; val fl[M], cp[M], flow; num cs[M], d[N], tot; int en = 2, n; int when = 0;
 	bool bfs(int s, int t) {
-		seen[t] = ++tempo; lv[t] = 0; int ql = 0, qr = 0; qu[qr++] = t;
+		seen[t] = ++when; lv[t] = 0; int ql = 0, qr = 0; qu[qr++] = t;
 		while(ql != qr) {
 			t = qu[ql++]; ei[t] = hd[t]; if(s == t) return true;
-			for(int e = hd[t]; e; e = nx[e]) if(seen[to[e]] != tempo && cp[e ^ 1] - fl[e ^ 1] > eps) {
-				seen[to[e]] = tempo;
+			for(int e = hd[t]; e; e = nx[e]) if(seen[to[e]] != when && cp[e ^ 1] - fl[e ^ 1] > eps) {
+				seen[to[e]] = when;
 				lv[to[e]] = lv[t] + 1;
 				qu[qr++] = to[e];
 			}
@@ -17,7 +17,7 @@ struct cycle_cancel {
 	}
 	val dfs(int s, int t, val f) {
 		if(s == t) return f;
-		for(int &e = ei[s]; e; e = nx[e]) if(ei[to[e]] && seen[to[e]] == tempo && cp[e] - fl[e] > eps && lv[to[e]] == lv[s] - 1)
+		for(int &e = ei[s]; e; e = nx[e]) if(ei[to[e]] && seen[to[e]] == when && cp[e] - fl[e] > eps && lv[to[e]] == lv[s] - 1)
 			if(val rf = dfs(to[e], t, min(f, cp[e] - fl[e]))) {
 				fl[e] += rf;
 				fl[e ^ 1] -= rf;
@@ -26,25 +26,25 @@ struct cycle_cancel {
 		return 0;
 	}
 	bool spfa() {
-		tempo++; int a = 0, b = 0, u;
-		for(int i = 0; i < n; i++) { d[i] = 0; qu[b++] = i; seen[i] = tempo; ct[i] = 0; }
+		when++; int a = 0, b = 0, u;
+		for(int i = 0; i < n; i++) { d[i] = 0; qu[b++] = i; seen[i] = when; ct[i] = 0; }
 		while(a != b) {
 			u = qu[a++]; if(a == N) a = 0; seen[u] = 0;
 			if(ct[u]++ >= n + 1) { a--; break; }
 			for(int e = hd[u]; e; e = nx[e]) if(cp[e] - fl[e] > val(0) && d[u] + cs[e] < d[to[e]] - eps) {
 				d[to[e]] = d[u] + cs[e]; pai[to[e]] = e ^ 1;
-				if(seen[to[e]] < tempo) { seen[to[e]] = tempo; qu[b++] = to[e]; if(b == N) b = 0; }
+				if(seen[to[e]] < when) { seen[to[e]] = when; qu[b++] = to[e]; if(b == N) b = 0; }
 			}
 		}
 		if(a == b) return false;
 		val mn = numeric_limits<val>::max();
-		tempo++;
-		for(; seen[u] != tempo; u = to[pai[u]]) seen[u] = tempo;
-		for(int v = u; seen[v] != tempo + 1; v = to[pai[v]]) {
-			seen[v] = tempo + 1;
+		when++;
+		for(; seen[u] != when; u = to[pai[u]]) seen[u] = when;
+		for(int v = u; seen[v] != when + 1; v = to[pai[v]]) {
+			seen[v] = when + 1;
 			mn = min(mn, cp[pai[v] ^ 1] - fl[pai[v] ^ 1]);
 		}
-		for(int v = u; seen[v] == tempo + 1; v = to[pai[v]]) {
+		for(int v = u; seen[v] == when + 1; v = to[pai[v]]) {
 			seen[v] = 0;
 			fl[pai[v]] -= mn;
 			fl[pai[v] ^ 1] += mn;
