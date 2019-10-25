d0fd5d72007d48645f0b0789f42f9e7c450c038c
Pedro Henrique DeSousaLopes <pdesousa@student12.cse.nd.edu>
2019-10-24 23:44:16 -0400

    diofantine

--- code/bfs.cpp
+++ code/bfs.cpp
@@ -5,26 +5,19 @@ vector<vector<int> > g;
   
 void edge(int a, int b) 
 { 
-    g[a].pb(b); 
-  
+    g[a].pb(b);  
     // for undirected graph add this line 
     // g[b].pb(a); 
 } 
-  
 void bfs(int u) 
 { 
     queue<int> q; 
-  
     q.push(u); 
     v[u] = true; 
-  
     while (!q.empty()) { 
-  
         int f = q.front(); 
         q.pop(); 
-  
         cout << f << " "; 
-  
         // Enqueue all adjacent of f and mark them visited  
         for (auto i = g[f].begin(); i != g[f].end(); i++) { 
             if (!v[*i]) { 
